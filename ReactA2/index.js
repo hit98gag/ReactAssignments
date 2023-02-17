24d8c833d687c7a958834bb94ddac3a9800e866f
Hitesh Tiwari <72449162+hit98gag@users.noreply.github.com>
2023-02-17 15:42:14 +0530

    Add files via upload

---
+++ ReactA2/index.js
@@ -0,0 +1,15 @@
+
+const airbnb = <div className="container">
+<h1>The Evolution of Airbnb's Frontend</h1>
+<div className="logocont">
+    <p>@spikebrehum</p>
+    <div class="logo">
+        <i class="fa-brands fa-airbnb"></i>
+        <p>airbnb</p>
+    </div>
+</div>
+
+</div>
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(airbnb)
